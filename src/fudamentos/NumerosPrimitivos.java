b2ee8c222d031cd6030242d90e2d152f433d9236
joaocastro <joaocastro0429@gmail.com>
2026-05-26 19:43:08 -0300

    tipo primitivos

---
+++ src/fudamentos/NumerosPrimitivos.java
@@ -0,0 +1,21 @@
+package fudamentos;
+
+public class NumerosPrimitivos {
+    public static void main(String[] args) {
+        int  idade=38;
+        System.out.println("Idade: "+idade);
+
+        char sexo='M';
+        System.out.println("Sexo: "+sexo);
+
+        float peso=12.5f;
+        System.out.println("Peso: "+peso);
+
+        double peso2=12.5;
+        System.out.println("Peso: "+peso2);
+
+        boolean estudante = true;
+        System.out.println("Estudante: "+estudante);
+
+    }
+}
