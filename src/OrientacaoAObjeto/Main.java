ed8e6cf5e6946aef466a9e2a458e88790a542ca7
joaocastro <joaocastro0429@gmail.com>
2026-06-01 12:07:03 -0300

    add classe record

--- src/OrientacaoAObjeto/Main.java
+++ src/OrientacaoAObjeto/Main.java
@@ -3,7 +3,11 @@ package OrientacaoAObjeto;
 public class Main {
     public static void main(String[] args) {
         Cachorro meuCachorro = new Cachorro("Rex", "Labrador",3,false);
-        meuCachorro.dormir();
+        CachorroRecord meuCachorro2 =
+                new CachorroRecord("viralata1", "viralata", 3);
+
+        System.out.println(meuCachorro2.nome());
+        System.out.println(meuCachorro2.descricaoCompleta());
         meuCachorro.acordar();
         meuCachorro.fazerAniversario();
         System.out.println("O Cachorro foi adicionado com sucesso!");
